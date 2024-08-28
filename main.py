3ea517d7bf17c0672641399d27eca7f1a6f3301b
Schulti <phillipp-schultheiss@web.de>
2024-08-28 10:06:24 +0200

    Adjusted copied python skript to new skript task

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import sys
+from pathlib import Path
+
+def mode_selection() -> str:
+    """
+    Prompts the user to select a mode.
+    Returns:
+    str: The selected mode.
+    """
+    mode_selection: bool = True
+    while mode_selection:
+        mode: str = input("Modus auswählen: 0 um Bild umbenennen; 1 um Aufnahmedatum des umbenannten Bildes ändern; 2 um Name des umbenannten Bildes zu ändern: ")
+        if mode in ["0", "1","2"]:
+            mode_selection = False
+        else:
+            print("Ungültige Eingabe!")
+    return mode
+            
+if __name__=="__main__":
+    modus:str = mode_selection()
+    date:str = date_input()
+    name:str = file_name_input()
+
+    # Set storage directory of the python skript as current diretory
+    script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+    os.chdir(script_dir)
+    
+    if modus == "0":
+        picture_rename(date, name)
+    if modus == "1":
+        picture_edit_date(date)
+    if modus == "2":
+        picture_edit_name(name)
+    
+    input("Drücken Sie Enter zum Beenden...")
