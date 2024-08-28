3f9d5045d3dc1ff15795ccfb6b7e6284c1dbeceb
Schulti <phillipp-schultheiss@web.de>
2024-08-28 10:13:22 +0200

    Insert date and file_name input functions

--- main.py
+++ main.py
@@ -16,7 +16,37 @@ def mode_selection() -> str:
         else:
             print("Ungültige Eingabe!")
     return mode
-            
+
+def date_input() -> str:
+    """
+    Prompts the user to enter a date.
+    Returns:
+    str: The entered date.
+    """
+    valid_date_input: bool = False
+    while not valid_date_input:
+        date: str = input("Aufnahmedatum der Bilder eingeben (Format: JJMMTT): ")
+        if len(date) == 6 and date.isdigit():
+            valid_date_input = True
+        else:
+            print("Ungültige Eingabe!")
+    return date
+
+def file_name_input() -> str:
+    """
+    Prompts the user to enter a file name.
+    Returns:
+    str: The entered file name.
+    """
+    valid_file_name_input: bool = False
+    while not valid_file_name_input:
+        name: str = input("Projektidentifizierung eingeben: ")
+        if len(name) > 0:
+            valid_file_name_input = True
+        else:
+            print("Ungültige Eingabe!")
+    return name
+
 if __name__=="__main__":
     modus:str = mode_selection()
     date:str = date_input()
