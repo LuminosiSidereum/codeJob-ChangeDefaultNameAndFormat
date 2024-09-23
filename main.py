41429bd76a95fe538d08c8593f79543f6764de33
Schulti <phillipp-schultheiss@web.de>
2024-09-23 18:40:16 +0200

    Added Mode 3 which converts the heic files into tiff files and moves the original heic files into a subdirectory

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import sys
 from pathlib import Path
+import shutil
+from PIL import Image
+import pillow_heif
 
 accepted_file_extensions: dict[str,list[str]] ={
     "picture":[".jpg", ".jpeg", ".png", ".gif",".tif",".tiff",".heic"],
@@ -17,12 +20,12 @@ def mode_selection() -> str:
     """
     mode_selection: bool = True
     while mode_selection:
-        modelist: list[str] = ["0: Bild komplett neu benennen","1: Aufnahmedatum des umbenannten Bildes ändern","2: Name des umbenannten Bildes ändern"]
+        modelist: list[str] = ["0: Bild komplett neu benennen","1: Aufnahmedatum des umbenannten Bildes ändern","2: Name des umbenannten Bildes ändern","3: .HEIC -> .TIFF"]
         for i in modelist:
             print(i)
         mode: str = input("Modus auswählen: ")
         mode = mode.strip()
-        if mode in ["0", "1","2"]:
+        if mode in ["0", "1","2","3"]:
             mode_selection = False
         else:
             print("Ungültige Eingabe!")
@@ -125,6 +128,36 @@ def picture_edit_name(name:str):
             file.rename(new_file_name)
             print(f"Umbenannt: {file} -> {new_file_name}")
 
+def change_heic_to_tiff()->None:
+    """
+    Converts all HEIC files in the current directory to TIFF format.
+    This function scans the current directory for files with a .heic extension,
+    converts them to TIFF format using the pillow_heif and PIL libraries, and
+    saves the converted files with the same name but with a .tiff extension.
+    The original HEIC files are then moved to a subdirectory named "Orginaldateien (HEIC)".
+    Steps:
+    1. Iterate through all files in the current directory.
+    2. Check if the file has a .heic extension (case insensitive).
+    3. Create the "Orginaldateien (HEIC)" directory if it does not exist.
+    4. Convert the HEIC file to TIFF format.
+    5. Save the converted TIFF file with the same name but with a .tiff extension.
+    6. Move the original HEIC file to the "Orginaldateien (HEIC)" directory.
+    7. Print a message indicating the conversion of each file.
+    Returns:
+        None
+    """
+    
+    for os_file in os.listdir():
+        file: Path = Path(os_file)
+        if file.suffix.lower() == ".heic":
+            Path("Orginaldateien (HEIC)").mkdir(exist_ok=True)
+            new_file_name:str = file.stem + ".tiff"
+            heic_file = pillow_heif.read_heif(os_file)
+            image = Image.frombytes(heic_file.mode, heic_file.size, heic_file.data, "raw", heic_file.mode, heic_file.stride)
+            image.save(new_file_name, "tiff".upper())
+            shutil.move(file, "Orginaldateien (HEIC)")
+            print(f"Konvertiert: {file} -> {new_file_name}")
+
 if __name__=="__main__":
     print("Achtung: Allle Bilder im Ordner erhalten den gleichen Namen und das gleiche Aufnahmedatum!")
     modus:str = mode_selection()
@@ -149,4 +182,6 @@ if __name__=="__main__":
             picture_edit_name(name_2)
         except ValueError as e:
             print(e)
+    if modus == "3":
+        change_heic_to_tiff()
     input("Drücken Sie Enter zum Beenden...")
---
+++ requirements.bat
@@ -0,0 +1,1 @@
+python pip install pillow-heif
